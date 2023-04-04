40f0e66ba529da8aa739e4bb6dc6568e9b138db6
root <root@DESKTOP-QV81KPN>
2023-04-05 02:01:39 +0300

    add count_word function

---
+++ Lab2/task1/calculate.py
@@ -0,0 +1,16 @@
+import re
+
+def all_calc_sentences(txt:str) -> int:
+    reg = r'[.?!]+'
+    return len(re.findall(reg, txt))
+
+def non_declarative_sentences(txt:str) -> int:
+    reg = r'[!?]+'
+    return len(re.findall(reg, txt))
+
+def count_words(txt:str) -> int:
+    reg = r'[\w-]+'
+    return len(re.findall(reg, txt))
+
+def average_length_of_sentence(txt:str) ->int:
+    return count_words(text)/all_calc_sentences(text)
