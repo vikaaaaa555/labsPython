fd867e8eeddef3be3d0144987ee383827d443f2b
root <root@DESKTOP-QV81KPN>
2023-04-05 10:58:58 +0300

    Test commit. Fix main.py

--- Lab2/task1/calculate.py
+++ Lab2/task1/calculate.py
@@ -1,16 +1,20 @@
 import re
 
-def all_calc_sentences(txt:str) -> int:
+
+def all_calc_sentences(txt: str) -> int:
     reg = r'[.?!]+'
     return len(re.findall(reg, txt))
 
-def non_declarative_sentences(txt:str) -> int:
+
+def non_declarative_sentences(txt: str) -> int:
     reg = r'[!?]+'
     return len(re.findall(reg, txt))
 
-def count_words(txt:str) -> int:
+
+def count_words(txt: str) -> int:
     reg = r'[\w-]+'
     return len(re.findall(reg, txt))
 
-def average_length_of_sentence(txt:str) ->int:
-    return count_words(text)/all_calc_sentences(text)
+
+def average_length_of_sentence(txt: str) -> int:
+    return count_words(txt) / all_calc_sentences(txt)
