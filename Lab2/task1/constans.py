5389d26250f2690cfae3ef86dcc4a8ea6cc7c9a9
root <root@DESKTOP-QV81KPN>
2023-04-06 14:54:08 +0300

    add const EDWARD

--- Lab2/task1/constans.py
+++ Lab2/task1/constans.py
@@ -1,6 +1,7 @@
-import re
 
 WORDS = r'\w*[A-z]\w*'
 ALL_WORDS = r'[\w]+'
 NON_DECLARATIVE_SENTENCES = r'[!?]+'
 ALL_SENTENCES = r'[.!?]+'
+EDWARD = "Beautiful? It's a skin of a killer, Bella! I'm a killer."
+SOME_TEXT = "ahuha111 1111 1111ahuha111 ahdsh111jcddj"
