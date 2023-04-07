0f2d6525577320f139c1e34a0a6f5ce51f3e8fb6
root <root@DESKTOP-QV81KPN>
2023-04-07 19:06:36 +0300

    add delete_numbers function

--- Lab2/task1/calculate.py
+++ Lab2/task1/calculate.py
@@ -1,20 +1,30 @@
+import constans
 import re
 
 
-def all_calc_sentences(txt: str) -> int:
-    reg = r'[.?!]+'
-    return len(re.findall(reg, txt))
+def all_sentences(txt: str) -> int:
+    return len(re.findall(constans.ALL_SENTENCES, txt))
+#все предложения
 
 
-def non_declarative_sentences(txt: str) -> int:
-    reg = r'[!?]+'
-    return len(re.findall(reg, txt))
+def calc_non_declarative_sentences(txt: str) -> int:
+    return len(re.findall(constans.NON_DECLARATIVE_SENTENCES, txt))
+#неповествовательные
 
 
 def count_words(txt: str) -> int:
-    reg = r'[\w-]+'
-    return len(re.findall(reg, txt))
+    return len(re.findall(constans.WORDS, txt))
+#количество слов
 
 
 def average_length_of_sentence(txt: str) -> int:
-    return count_words(txt) / all_calc_sentences(txt)
+    return round(count_words(txt) / all_sentences(txt))
+#средняя длина предложения
+
+
+def delete_numbers(txt: str) -> str:
+    return re.sub(constans.NUMBERS, ' ', txt)
+#удалить числа
+
+
+def
