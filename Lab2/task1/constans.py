0c81784e8df9974dca46c22144a5e08227409674
root <root@DESKTOP-QV81KPN>
2023-04-07 20:02:18 +0300

    add NUMBERS, CHARACTERS

--- Lab2/task1/constans.py
+++ Lab2/task1/constans.py
@@ -1,6 +1,8 @@
 WORDS = r'\w*[A-z\']\w*'
-ALL_WORDS = r'[\w]+'
+ALL_WORDS = r'\w+'
 NON_DECLARATIVE_SENTENCES = r'[!?]+'
 ALL_SENTENCES = r'[.!?]+'
 EDWARD = "Beautiful? It's a skin of a killer, Bella! I'm a killer."
 SOME_TEXT = "ahuha111 1111 1111ahuha111 ahdsh111jcddj"
+NUMBERS = "\s\d+\s"
+CHARACTERS = r'\w'
