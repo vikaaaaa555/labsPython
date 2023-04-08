95f8259e62e9f5253ad126e02055eb001857effa
root <root@DESKTOP-QV81KPN>
2023-04-08 12:47:51 +0300

    add abbrivation constans

--- Lab2/task1/constans.py
+++ Lab2/task1/constans.py
@@ -1,8 +1,18 @@
-WORDS = r'\w*[A-z\']\w*'
-ALL_WORDS = r'\w+'
-NON_DECLARATIVE_SENTENCES = r'[!?]+'
-ALL_SENTENCES = r'[.!?]+'
+WORDS_PATTERN = r'\w*[A-z\']\w*'
+ALL_WORDS_PATTERN = r'\w+'
+NON_DECLARATIVE_SENTENCES_PATTERN = r'[!?]+'
+ALL_SENTENCES_PATTERN = r'[.!?]+'
+NUMBERS_PATTERN = r'\s\d+\s'
+CHARACTERS_PATTERN = r'\w'
 EDWARD = "Beautiful? It's a skin of a killer, Bella! I'm a killer."
 SOME_TEXT = "ahuha111 1111 1111ahuha111 ahdsh111jcddj"
-NUMBERS = "\s\d+\s"
-CHARACTERS = r'\w'
+
+ONE_LETTERS_ABBRIVATION = (
+    r'Mr\.', r'Ms\.', r'Mrs\.', r'Dr\.', r'Jr\.',
+    r'Sr\.', r'ft\.', r'etc\.', r'inc\.', r'yr\.',
+    r'Smth\.', r'Smb\.'
+)
+TWO_LETTERS_ABBRIVATION = (
+    r'i\.e\.', r'e\.g\.', r'a\.d\.', r'b\.c\.',
+    r'c\.e\.', r'b\.c\.e\.', r'a\.m\.', r'p\.m\.', r'vs\.'
+)
