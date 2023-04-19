fe1f38823c4a77bc7c891ed43275e5696ccf8eae
root <root@DESKTOP-QV81KPN>
2023-04-20 00:41:49 +0300

    add remove func

--- Lab2/task2/container.py
+++ Lab2/task2/container.py
@@ -6,6 +6,7 @@ class UniqueContainer:
     def __int__(self, username):
         self.username = username
         self.data = {}
+        self.loaded = False
 
 
     def add(self, *args):
@@ -17,5 +18,14 @@ class UniqueContainer:
                     self.data[self.username].append(element)
 
 
-    def remove(self, key):
-        self.elements.discard(key)
+    def remove(self, arg):
+        self.username.discard(arg)
+        if self.username not in self.data:
+            print("Container is empty")
+        elif arg not in self.data:
+            print("These elements are not in the container")
+        else:
+            self.data[self.username].remove(arg)
+
+
+    def find(self, *args):
