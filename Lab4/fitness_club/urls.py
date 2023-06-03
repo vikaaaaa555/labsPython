aad77813aac013a32dae70a8ad1816af62caf965
root <root@DESKTOP-QV81KPN>
2023-06-03 23:54:01 +0300

    added index path

--- Lab4/fitness_club/urls.py
+++ Lab4/fitness_club/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from fitness_club.tests import views
+from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', views.index, name="index"),
 ]
