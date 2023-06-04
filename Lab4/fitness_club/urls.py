1d67a1d63f618f914c03f3d0d2c2fcb7fc05ebdd
root <root@DESKTOP-QV81KPN>
2023-06-04 16:58:00 +0300

    added club_cards path

--- Lab4/fitness_club/urls.py
+++ Lab4/fitness_club/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.index, name='index'),
+    path('club_cards', views.club_cards, name='club_cards')
 ]
