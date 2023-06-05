4221799adf4af7880c454e53e7eb8aa2271a0911
root <root@DESKTOP-QV81KPN>
2023-06-05 17:49:39 +0300

    added STATICFILES_DIRS

--- Lab4/Lab4/settings.py
+++ Lab4/Lab4/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'fitness_club/static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
